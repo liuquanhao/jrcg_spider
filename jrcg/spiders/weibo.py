5833ed0d9b5243d7cd32d534345336f2bbf55926
liuxu <i@liuxu.me>
2019-05-09 17:24:57 +0800

    删除多余的段

--- jrcg/spiders/weibo.py
+++ jrcg/spiders/weibo.py
@@ -10,7 +10,6 @@ class WeiboSpider(scrapy.Spider):
 
     def parse(self, response):
         tr_list = response.xpath("//*[@id='pl_top_realtimehot']/table/tbody/tr")
-        now = int(time.time())
         for index, tr in enumerate(tr_list):
             weibo = JrcgItem()
             if index == 0:
@@ -21,6 +20,5 @@ class WeiboSpider(scrapy.Spider):
             weibo['link'] = "https://s.weibo.com" + tr.xpath(".//td[position()=2]/a/@href").extract_first(default = '-')
             weibo['count'] = int(tr.xpath(".//td[position()=2]/span/text()").extract_first(default = 0))
             weibo['state'] = tr.xpath(".//td[position()=3]/i/text()").extract_first(default = '')
-            weibo['insert_time'] = now
             weibo['name'] = 'weibo'
             yield weibo
