ac516f39dce53bf8c8124b658e784b09939aa34e
liuxu <i@liuxu.me>
2019-07-24 15:44:40 +0800

    微信爬虫添加代理

--- jrcg/spiders/weixin_24hrewendang.py
+++ jrcg/spiders/weixin_24hrewendang.py
@@ -5,14 +5,18 @@ from jrcg.items import JrcgItem
 class Weixin24hrewenbangSpider(scrapy.Spider):
     name = 'weixin_24hrewenbang'
     allowed_domains = ['gsdata.cn']
-    start_urls = ['http://www.gsdata.cn/rank/wxarc']
+
+    def start_requests(self):
+        request = scrapy.Request(url='http://www.gsdata.cn/rank/wxarc')
+        request.meta['proxy'] = 'http://127.0.0.1:8081'
+        yield request
 
     def parse(self, response):
-       tr_list = response.xpath('.//table[@id="rank_data"]/tbody/tr') 
-       for index, tr in enumerate(tr_list):
-           jrcg = JrcgItem()
-           jrcg['rank'] = index + 1
-           jrcg['title'] = tr.xpath('.//td[1]/span/a/text()').extract_first()
-           jrcg['link'] = tr.xpath('.//td[1]/span/a/@href').extract_first()
-           jrcg['name'] = self.name 
-           yield jrcg
+        tr_list = response.xpath('.//table[@id="rank_data"]/tbody/tr') 
+        for index, tr in enumerate(tr_list):
+            jrcg = JrcgItem()
+            jrcg['rank'] = index + 1
+            jrcg['title'] = tr.xpath('.//td[1]/span/a/text()').extract_first()
+            jrcg['link'] = tr.xpath('.//td[1]/span/a/@href').extract_first()
+            jrcg['name'] = self.name 
+            yield jrcg
